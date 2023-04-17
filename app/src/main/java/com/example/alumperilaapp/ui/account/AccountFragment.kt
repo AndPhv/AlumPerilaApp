457b3b1dc3e41aba51f7e605975c1f5a307b61a8
AndPhv <puhov2001@gmail.com>
2023-04-17 23:06:44 +0300

    Корретировки

--- app/src/main/java/com/example/alumperilaapp/ui/account/AccountFragment.kt
+++ app/src/main/java/com/example/alumperilaapp/ui/account/AccountFragment.kt
@@ -4,36 +4,21 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProvider
+import androidx.fragment.app.viewModels
 import com.example.alumperilaapp.databinding.FragmentAccountBinding
 
 class AccountFragment : Fragment() {
 
-    private var _binding: FragmentAccountBinding? = null
-    private val binding get() = _binding!!
+    private lateinit var binding: FragmentAccountBinding
+    private val vm: AccountViewModel by viewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val notificationsViewModel =
-            ViewModelProvider(this).get(AccountViewModel::class.java)
-
-        _binding = FragmentAccountBinding.inflate(inflater, container, false)
-        val root: View = binding.root
-
-        val textView: TextView = binding.textAccount
-        notificationsViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
-        }
-        return root
-    }
-
-    override fun onDestroyView() {
-        super.onDestroyView()
-        _binding = null
+        binding = FragmentAccountBinding.inflate(inflater, container, false)
+        return binding.root
     }
 }
--- app/src/main/java/com/example/alumperilaapp/ui/account/AccountViewModel.kt
+++ app/src/main/java/com/example/alumperilaapp/ui/account/AccountViewModel.kt
@@ -1,13 +1,7 @@
 package com.example.alumperilaapp.ui.account
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class AccountViewModel : ViewModel() {
 
-    private val _text = MutableLiveData<String>().apply {
-        value = "This is account Fragment"
-    }
-    val text: LiveData<String> = _text
 }
