bac81a89a00bccfdb84304595593132493c45b84
AndPhv <puhov2001@gmail.com>
2023-04-18 11:17:47 +0300

    Работа с toolbar и systembar

--- app/src/main/java/com/example/alumperilaapp/MainActivity.kt
+++ app/src/main/java/com/example/alumperilaapp/MainActivity.kt
@@ -1,9 +1,13 @@
 package com.example.alumperilaapp
 
 import android.os.Bundle
+import android.view.View.GONE
+import android.view.View.VISIBLE
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
 import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
@@ -12,6 +16,8 @@ import com.example.alumperilaapp.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private lateinit var navHostFragment: NavHostFragment
+    private lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,15 +26,29 @@ class MainActivity : AppCompatActivity() {
 
         val navView: BottomNavigationView = binding.navView
 
-        val navController = findNavController(R.id.nav_host_fragment_activity_main)
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
+        navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment_activity_main) as NavHostFragment
+        navController = navHostFragment.navController
+
+        setSupportActionBar(binding.toolbar)
+
         val appBarConfiguration = AppBarConfiguration(
             setOf(
                 R.id.navigation_catalog, R.id.navigation_home, R.id.navigation_account
             )
         )
+
+        navController.addOnDestinationChangedListener{_,distonation, _ ->
+            when(distonation.id) {
+                R.id.navigation_catalog -> {
+                    binding.toolbar.visibility = VISIBLE
+                }
+                else ->
+                    binding.toolbar.visibility = GONE
+            }
+        }
+
         setupActionBarWithNavController(navController, appBarConfiguration)
-        navView.setupWithNavController(navController)
+        binding.navView.setupWithNavController(navController)
     }
 }
